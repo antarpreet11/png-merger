f0c88452884f705c0688bdae4e3782ac64555806
j8bennet <j8bennet@uwaterloo.ca>
2024-05-16 11:14:51 -0400

    added is_png function

--- lab1/source/pnginfo.c
+++ lab1/source/pnginfo.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
-#include "../starter/png_util/lab_png.h"
+#include "lab_png.h"
+
+int is_png(U8 *buf, size_t n) {
+	U8 compare[8] = {'89', '50', '4E', '47', '0D', '0A', '1A', '0A'};
+	printf("%s\n", buf);
+	return memcmp(buf, compare, n);
+}
 
 int pnginfo(const char *buf) {
 	FILE *img = fopen(buf, "r");
 	char content;
 
+	U8 header[8];
+	fread(header, 8, 1, img);
+	printf("%s\n", header);
+	int isPNG = is_png(header, 8);
+	/*
 	while(1) {
 		content = fgetc(img);
 
@@ -12,13 +23,13 @@ int pnginfo(const char *buf) {
 
 		printf("%c", content);	
 	}
-
+	*/
 	fclose(img);
 
 	return 0;
 }
 
-int main(const char *buf) {
-  pnginfo(buf);
-  return 0;
+int main(int argc, char **argv) {
+	pnginfo(argv[1]);
+	return 0;
 }
