35acb2b827cca135aeb408d629ab93dc5223a217
Antarpreet Singh Khalsa <as2khals@uwaterloo.ca>
2024-05-24 14:32:15 -0400

    Write png function

--- lab1/source/catpng.c
+++ lab1/source/catpng.c
@@ -1,5 +1,49 @@
+#include <arpa/inet.h>
 #include "catpng.h"
 #include "lab_png.h"
+#include "zlib.h"
+
+void write_png_file(const char *filename, struct simple_PNG *new_png) {
+    FILE *fp = fopen(filename, "wb+");
+    if (fp == NULL) {
+        fprintf(stderr, "write_png_file: Could not open file %s for writing\n", filename);
+        return;
+    }
+
+    U8 header[] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+
+    /* Write PNG signature */
+    fwrite(header, 1, PNG_SIG_SIZE, fp);
+
+    /* Write IHDR chunk */
+    write_chunk(fp, new_png->p_IHDR);
+
+    /* Write IDAT chunk */
+    write_chunk(fp, new_png->p_IDAT);
+
+    /* Write IEND chunk */
+    write_chunk(fp, new_png->p_IEND);
+
+    fclose(fp);  
+}
+
+void write_chunk(FILE *fp, chunk_p new_chunk) {
+    new_chunk->length = htonl(new_chunk->length);
+
+    fwrite(&(new_chunk->length), sizeof(U32), 1, fp);
+    fwrite(new_chunk->type, sizeof(U8), CHUNK_TYPE_SIZE, fp);
+    if (new_chunk->p_data != NULL && new_chunk->length > 0) {
+        fwrite(new_chunk->p_data, sizeof(U8), new_chunk->length, fp);
+    }
+
+    unsigned long crc = crc32(0L, Z_NULL, 0);
+    crc = crc32(crc, (const Bytef *)new_chunk->type, CHUNK_TYPE_SIZE);
+    if (new_chunk->p_data != NULL && new_chunk->length > 0) {
+        crc = crc32(crc, new_chunk->p_data, new_chunk->length);
+    }
+    new_chunk->crc = htonl(crc);
+    fwrite(&(new_chunk->crc), sizeof(U32), 1, fp);
+}
 
 int catpng(char **buf, int count) {
     simple_PNG_p pngIn[count];
