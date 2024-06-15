b39aabf2fbc2242ea0f17bc643153c1f91ec9706
Antarpreet Singh <84466072+antarpreet11@users.noreply.github.com>
2024-06-15 18:21:11 -0400

    Delete lab1 directory

--- lab1/source/writepngtest.c
+++
@@ -1,9 +0,0 @@
-#include "catpng.h"
-#include "lab_png.h"
-#include <stdio.h> 
-
-int main(int argc, char **argv) {
-    simple_PNG_p pngin = pnginfo(argv[1]);
-    printf("Png info extracted\n");
-    write_png_file("all.png", pngin);
-}
